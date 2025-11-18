3b4003d909978f3c70008ba0454e0002749f0731
HyroVitalyProtago <hyrovitalyprotago@gmail.com>
2025-11-19 00:23:27 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -7,6 +7,35 @@ const __dirname = path.dirname(__filename)
 
 const app = express()
 
+const links = {
+  samples: [
+    { url:'https://webxr.vercel.com', date }
+  ]
+}
+app.post('/:room', (req, res) => {
+  const { link } = req.body;
+  if (!link)
+    return res.status(400).json({ error: 'Link is required' });
+
+  const newLink = { link, date: new Date() }
+
+  if (!links[req.params.room]) {
+    links[req.params.room] = []
+  }
+
+  links[req.params.room].push(newLink)
+
+  res.status(201).json(newLink);
+});
+
+// app.delete('/:room/:url', (req, res) => {
+//   const index = todos.findIndex(t => t.id === parseInt(req.params.id));
+//   if (index === -1) return res.status(404).json({ error: 'Todo not found' });
+
+//   todos.splice(index, 1);
+//   res.status(204).send();
+// });
+
 // Home route - HTML
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'components', 'index.html'))
@@ -15,6 +44,14 @@ app.get('/', (req, res) => {
 app.get('/about', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'components', 'about.html'))
 })
+
+// show all links that belongs to that room
+app.get('/room/:room', function (req, res) {
+  res.render(
+    path.join(__dirname, '..', 'components', 'room.ejs'),
+    { room: req.params.room, data: links[req.params.room] ?? [] }
+  );
+});
 
 // Example API endpoint - JSON
 app.get('/api-data', (req, res) => {
@@ -28,5 +65,7 @@ app.get('/api-data', (req, res) => {
 app.get('/healthz', (req, res) => {
   res.status(200).json({ status: 'ok', timestamp: new Date().toISOString() })
 })
+
+
 
 export default app
